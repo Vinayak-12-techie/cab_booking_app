32deb0f54399f470a8fa48aec9f8e2ada28df74a
Your Name <Vinayak Karshni>
2024-11-24 11:56:19 +0530

    Added Email Sending Service

--- uber/src/test/java/com/vinayak/project/uber/uberApp/UberApplicationTests.java
+++ uber/src/test/java/com/vinayak/project/uber/uberApp/UberApplicationTests.java
@@ -1,13 +1,22 @@
 package com.vinayak.project.uber.uberApp;
 
+import com.vinayak.project.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
+
+
+
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail("wetenay900@merotx.com","Test Email","Hello There , this is a test Email");
 	}
 
 }
